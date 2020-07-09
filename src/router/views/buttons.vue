fa6ff7a1dd2f961352ad5bb909a51d7e240bb9be
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-07-09 22:41:22 +0600

    Add route for button view

---
+++ src/router/views/buttons.vue
@@ -0,0 +1,21 @@
+<script>
+import Layout from '@layouts/main.vue'
+
+export default {
+  page: {
+    title: 'Buttons',
+    meta: [{ name: 'description', content: 'The Buttons page.' }],
+  },
+  components: { Layout },
+}
+</script>
+
+<template>
+  <Layout>
+    Buttons
+  </Layout>
+</template>
+
+<style lang="scss" module>
+@import '@design';
+</style>

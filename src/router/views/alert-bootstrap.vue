5d9a14afd8f782e567d198e70dd364ea13d2a7fd
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-07-19 11:40:46 +0600

    Additional content inside alerts

--- src/router/views/alert-bootstrap.vue
+++ src/router/views/alert-bootstrap.vue
@@ -92,6 +92,24 @@ export default {
         <b-col></b-col>
       </b-row>
     </b-container>
+    <b-container>
+      <h2>#Additional content inside alerts</h2>
+      <div>
+        <b-alert show variant="info">
+          <h4 class="alert-heading">Well done!</h4>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p class="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </b-alert>
+      </div>
+    </b-container>
   </Layout>
 </template>
 
